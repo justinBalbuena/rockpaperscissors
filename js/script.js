c86b879c8e4ff3c42d0ab11e15aca4117a6a0ed2
Glitch (rock-paper-scissors-codenation) <none>
2022-01-20 23:26:55 +0000

    🐧🏢 Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -1,1 +1,58 @@
+function generateComputerChoice (){
+  let computerChoice = Math.floor((Math.random() * 3) + 1);
+    if (computerChoice === 1) {
+      computerChoice = "ROCK";
+      $(".computerChoice").text("ROCK");
+    }
+    else if (computerChoice === 2) {
+      computerChoice = "SCISSORS";
+      $(".computerChoice").text("SCISSORS");
+    }
+    else {
+      computerChoice = "PAPER";
+      $(".computerChoice").text("PAPER");
+    }
+  return computerChoice;
+}
 
+function determineWinner (choice, computerChoice) {
+  if (choice === computerChoice) {
+      $(".result").text("Tie! " + "Play Again!");
+      $(".computerScore").text("Computer: " + computerScore);
+      $(".playerScore").text("Player: " + playerScore);
+    }
+    else if ( 
+      ((choice === "ROCK") && (computerChoice === "SCISSORS")) ||
+      ((choice === "SCISSORS") && (computerChoice === "PAPER")) ||
+      ((choice === "PAPER") && (computerChoice === "ROCK")) ) 
+    {
+      $(".result").text("You win! " + "Play Again!");
+      playerScore++;
+      $(".computerScore").text("Computer: " + computerScore);
+      $(".playerScore").text("Player: " + playerScore);
+    }
+    else {
+      $(".result").text("You Lose! " + "Play Again!");
+      computerScore++;
+      $(".computerScore").text("Computer: " + computerScore);
+      $(".playerScore").text("Player: " + playerScore);
+    }
+}
+
+let playerScore = 0;
+let computerScore = 0;
+$(".scores").hide();
+
+$(".play").click (function() {
+  let choice = $(".input").val().toUpperCase();
+  $(".userChoice").text(choice);
+  if (choice === "SCISSORS" || choice === "ROCK" || choice === "PAPER") {
+    let computerChoice = generateComputerChoice();
+    determineWinner(choice, computerChoice);
+    $(".scores").show();
+  }
+  else {
+    $(".result").text("This is not a valid answer >:(");
+  }
+  
+});
